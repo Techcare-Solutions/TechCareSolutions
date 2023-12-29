0a2aeb66e548bccab8d6d4b01a3590a8c71ad542
Luca Dandois <luca.dandois@student.kdg.be>
2023-12-29 22:57:00 +0100

    second commit

--- js/navbar/navbar.js
+++ js/navbar/navbar.js
@@ -1,14 +1,26 @@
-function loadNavbar() {
-    let navbar = document.getElementById('navbar');
-    let activeId = navbar.innerHTML;
-
+window.onload = function () {
     fetch('navbar.html')
         .then(response => response.text())
         .then(data => {
-            navbar.innerHTML = data;
+            data = setActiveClass(data);
+            document.getElementById('navbar').innerHTML = data;
         })
-    let active = document.getElementById(activeId);
-    active.classList.add('active');
 }
 
-loadNavbar();
+function setActiveClass(data) {
+    const path = window.location.pathname;
+    const page = path.split("/").pop();
+
+    switch (page) {
+        case 'portfolio.html':
+            const regex = /id="portfolioDropdown" class="([^"]*)"/;
+            const match = data.match(regex);
+            if (match && match[1]) {
+                const currentClasses = match[1];
+                data = data.replace(regex, `id="portfolioDropdown" class="${currentClasses} active"`);
+            }
+            break;
+    }
+
+    return data;
+}
